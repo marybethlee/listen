6ff31628d67b7c0ac7db5bc5eb271dcbaea26f77
Mary Lee <marybethlee11@gmail.com>
2019-10-04 11:52:59 -0400

    Remove unnecessary timeout on listen

--- models/database_connection.rb
+++ models/database_connection.rb
@@ -3,8 +3,8 @@ require "sequel"
 class DatabaseConnection
   DB = Sequel.connect(ENV["DATABASE_URL"] || "postgres://localhost:5432/listen_development")
 
-  def self.listen(timeout = 600, &block)
-    DB.listen('comments', loop: true, timeout: timeout) do |_channel, _pid, event_data|
+  def self.listen(&block)
+    DB.listen('comments', loop: true) do |_channel, _pid, event_data|
       data = JSON.parse(event_data)
       yield data.dig("record", "body")
     end
